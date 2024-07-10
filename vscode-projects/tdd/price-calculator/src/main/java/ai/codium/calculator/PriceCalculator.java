556e36472b4cb70f3d351c7e87e07dc79602ec7d
davidparry <david.p@codium.ai>
2024-07-10 18:05:33 -0500

    vscode setup for getting started tutorial

---
+++ vscode-projects/tdd/price-calculator/src/main/java/ai/codium/calculator/PriceCalculator.java
@@ -0,0 +1,7 @@
+package ai.codium.calculator;
+
+public class PriceCalculator {
+    public static double calculateDiscountedPrice(double originalPrice, double discountPercentage) {
+        return Double.MIN_VALUE;
+      }
+}
