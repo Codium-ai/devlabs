6cc2ac7066c96f408dff42d975ef10d4d3d4f5da
David Parry <davidparry@users.noreply.github.com>
2024-08-29 14:31:18 -0500

    basic project creation with a controller (#17)
    Co-authored-by: davidparry <david.p@codium.ai>

---
+++ intellij-projects/pr-agent/src/main/java/ai/codium/PrAgentApp.java
@@ -0,0 +1,13 @@
+package ai.codium;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrAgentApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PrAgentApp.class, args);
+    }
+}
